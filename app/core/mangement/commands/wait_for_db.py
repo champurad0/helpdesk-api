46fa0254a73a412c6b597aecb2d85c9570d95115
champurad0 <124223013+champurad0@users.noreply.github.com>
2024-07-15 14:31:10 -0700

    added tests

--- app/core/mangement/commands/wait_for_db.py
+++
@@ -1,9 +0,0 @@
-"""
-Django command to wait for db 
-"""
-from django.core.mangement.base import BaseCommand 
-
-class Command(BaseCommand):
-    """ Django command to wait for db """"
-    def handle (self, *args, **options):
-        pass 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,29 +3,35 @@ Test custom Django management commands.
 """
 
 from unittest.mock import patch
-from psycopg2 import OperationalError as Pyscopg2Error
 
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from core.management.commands import wait_for_db
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
 @patch("core.management.commands.wait_for_db.Command.check")
-class CommandTest(SimpleTestCase):
-    """Test Commands"""
+class CommandTests(SimpleTestCase):
+    """Test commands."""
 
-    def test_wait_for_db(self, patched_check):
-        """Test Waiting for db status"""
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
+
         call_command("wait_for_db")
-        patched_check.assert_called_once_with(database=["default"])
+
+        patched_check.assert_called_once_with(databases=["default"])
 
     @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """test Waiting for database when getting error"""
+        """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = (
-            [Pyscopg2Error] * 2 + [OperationalError] * 3 + [True]
+            [Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
         )
+
         call_command("wait_for_db")
+
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=["default"])
+        patched_check.assert_called_with(databases=["default"])
