1659e2edf17123d558150373e321c9e79a25107b
champurad0 <124223013+champurad0@users.noreply.github.com>
2024-07-15 13:50:54 -0700

    added unit test for db

---
+++ app/core/mangement/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+Django command to wait for db 
+"""
+from django.core.mangement.base import BaseCommand 
+
+class Command(BaseCommand):
+    """ Django command to wait for db """"
+    def handle (self, *args, **options):
+        pass 
