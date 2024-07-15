852c64a1246c68eaa00f51448308a683240f0be5
champurad0 <124223013+champurad0@users.noreply.github.com>
2024-07-15 14:38:34 -0700

    corrected tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,6 @@ from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
-from core.management.commands import wait_for_db
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
