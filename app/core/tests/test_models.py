cddb33aa359ee6b7cc03c52258feaff9e4dade52
champurad0 <124223013+champurad0@users.noreply.github.com>
2024-07-16 13:40:58 -0700

    added admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,3 +31,16 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """test that creating a user without an email raises an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
+
+    def test_create_superuser(self):
+        """testing creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "password1"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
